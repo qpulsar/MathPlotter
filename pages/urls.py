1dae5c0612c6c8ec90185d7042e832ebe4eea2d0
qpulsar <korkusuz@balikesir.edu.tr>
2025-01-17 14:48:03 +0300

    blocks

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,31 @@
 from django.urls import path
-from pages.views import HomePageView
+from django.contrib.auth import views as auth_views
+from pages.views import HomePageView, LoginView, RegisterView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),  # Ana sayfa için URL
-    # Diğer URL'ler...
-] 
+    path('login/', LoginView.as_view(), name='login'),  # Login sayfası için URL
+    path('register/', RegisterView.as_view(), name='register'),
+    
+    # Password Reset URLs
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html',
+        email_template_name='registration/password_reset_email.html',
+        subject_template_name='registration/password_reset_subject.txt'
+    ), name='password_reset'),
+    
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'
+    ), name='password_reset_done'),
+    
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='registration/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+    
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'
+    ), name='password_reset_complete'),
+    
+    # Logout URL
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,25 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView as AuthLoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
-class HomePageView(View):
-    def get(self, request):
-        return render(request, 'main.html')
+class HomePageView(TemplateView):
+    template_name = 'maker.html'
 
+class LoginView(AuthLoginView):
+    template_name = 'login.html'
+
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return response
